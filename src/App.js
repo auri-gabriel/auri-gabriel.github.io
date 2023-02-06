7d25bdc01528d16c8548dc074985635954732555
Auri Gabriel <ririmello.a@gmail.com>
2023-02-06 15:12:22 -0300

    empty navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Hero from './Hero';
 import Skills from './Skills';
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <Skills />
       <Footer />
