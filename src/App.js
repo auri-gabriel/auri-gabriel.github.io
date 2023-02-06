f906be060f8ef43128333b6289c2f4e5b7e9e4cf
Auri Gabriel <ririmello.a@gmail.com>
2023-02-06 05:15:42 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
 import Skills from './Skills';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <>
       <Hero />
       <Skills />
+      <Footer />
     </>
   );
 };
