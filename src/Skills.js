961259b078580eeec668efe62879f66b7bc946c4
Auri Gabriel <ririmello.a@gmail.com>
2023-02-06 05:18:37 -0300

    removed unused text

--- src/Skills.js
+++ src/Skills.js
@@ -69,11 +69,11 @@ export default function Skills() {
           <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
             The technologies I use
           </p>
-          <p className='mt-6 text-lg leading-8 text-gray-600'>
+          {/* <p className='mt-6 text-lg leading-8 text-gray-600'>
             Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
             Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
             In mi viverra elit nunc.
-          </p>
+          </p> */}
         </div>
         <div className='mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl'>
           <dl className='grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-2 lg:gap-y-16'>
