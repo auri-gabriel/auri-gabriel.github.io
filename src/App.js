ba769b1d6d226857449c4dc995039c020c4657aa
auri-gabriel <ririmello.a@gmail.com>
2023-02-06 19:43:00 -0300

    remove the navbar as it is useless right now

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Hero from './Hero';
 import Skills from './Skills';
 import Footer from './Footer';
-import Navbar from './Navbar';
 import Container from './Layout/Container';
 
 const App = () => {
   return (
     <>
-      {/* <Navbar /> */}
       <Hero />
       <Container>
         <Skills />
