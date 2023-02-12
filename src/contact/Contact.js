d4c079ab2f3a7c3ce6b077fb0489a546f48f8b45
auri-gabriel <ririmello.a@gmail.com>
2023-02-12 16:03:55 -0300

    better submit error handling

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -29,8 +29,14 @@ const Contact = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        setSuccess(true);
-        console.log(data);
+        if (data.error) {
+          setError(true);
+          console.log(data.error);
+        }
+        if (data.ok) {
+          setSuccess(true);
+          console.log(data);
+        }
       })
       .catch((err) => {
         setError(true);
@@ -42,52 +48,91 @@ const Contact = () => {
   };
 
   return (
-    <section id='contact' className='py-20'>
+    <section
+      id='contact'
+      className='
+    w-full
+    h-screen
+    mx-auto
+    text-center
+    flex
+    flex-col
+    justify-between'
+    >
       <div className='container mx-auto'>
         <h2 className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-4'>
           Get in Touch
         </h2>
-        <form onSubmit={handleFormSubmit}>
-          <div className='mb-4'>
-            <TextField
-              labelText='Name'
-              type='text'
-              name='name'
-              id='name'
-              value={formData.name}
-              onChange={handleInputChange}
-              className='w-full border border-gray-400 p-2'
-            />
+        <div className='flex lg:flex-row'>
+          <div className='lg:w-1/2 px-6'>
+            <div className='p-6 bg-white shadow-md'>
+              <h3 className='text-lg font-medium mb-4'>Social Links</h3>
+              <p className='mb-2'>
+                <a
+                  href='#'
+                  className='inline-block mr-4 text-gray-600 hover:text-gray-800'
+                >
+                  Facebook
+                </a>
+                <a
+                  href='#'
+                  className='inline-block mr-4 text-gray-600 hover:text-gray-800'
+                >
+                  Twitter
+                </a>
+                <a
+                  href='#'
+                  className='inline-block text-gray-600 hover:text-gray-800'
+                >
+                  LinkedIn
+                </a>
+              </p>
+            </div>
           </div>
-          <div className='mb-4'>
-            <TextField
-              labelText='Email'
-              type='email'
-              name='email'
-              id='email'
-              value={formData.email}
-              onChange={handleInputChange}
-              className='w-full border border-gray-400 p-2'
-            />
+          <div className='lg:w-1/2 px-6'>
+            <form onSubmit={handleFormSubmit}>
+              <div className='mb-4'>
+                <TextField
+                  labelText='Name'
+                  type='text'
+                  name='name'
+                  id='name'
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  className='w-full border border-gray-400 p-2'
+                />
+              </div>
+              <div className='mb-4'>
+                <TextField
+                  labelText='Email'
+                  type='email'
+                  name='email'
+                  id='email'
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  className='w-full border border-gray-400 p-2'
+                />
+              </div>
+              <div className='mb-4'>
+                <TextArea
+                  labelText='Message'
+                  name='message'
+                  id='message'
+                  value={formData.message}
+                  onChange={handleInputChange}
+                  className='w-full border border-gray-400 p-2'
+                />
+              </div>
+              <div className='text-center'>
+                <SubmitButton
+                  isLoading={isLoading}
+                  success={success}
+                  error={error}
+                />
+              </div>
+            </form>
           </div>
-          <div className='mb-4'>
-            <TextArea
-              labelText='Message'
-              name='message'
-              id='message'
-              value={formData.message}
-              onChange={handleInputChange}
-              className='w-full border border-gray-400 p-2'
-            />
-          </div>
-          <div className='text-center'>
-            <SubmitButton
-              isLoading={isLoading}
-              success={success}
-              error={error}
-            />
-          </div>
-        </form>
+        </div>
       </div>
     </section>
   );
